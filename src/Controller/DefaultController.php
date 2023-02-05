b3bf2a11ddfe21ecb7999abf0a1719a8f6314fe3
valmri <valmarmie87@outlook.com>
2023-02-05 16:03:26 +0100

    Utilisation de la configuration

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Configuration;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('default/index.html.twig');
+        $configuration = $entityManager->find(Configuration::class, 1);
+
+        return $this->render('default/index.html.twig', array(
+            "configuration" => $configuration
+        ));
     }
 }
